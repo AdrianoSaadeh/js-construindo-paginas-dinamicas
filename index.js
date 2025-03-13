6ce0efca369691f0fd9cd6f34ee954883f78241d
Adriano Saadeh <adrianosaadeh@gmail.com>
2025-03-13 07:58:40 -0300

    adiciona data e hora a itens do checklis

--- index.js
+++ index.js
@@ -19,12 +19,28 @@ botaoAdicionar.addEventListener('click', (evento) => {
     const inputCheckbox = document.createElement("input");
     inputCheckbox.type = "checkbox";
     inputCheckbox.id = "checkbox-" + ++contador;
-    
+
     const nomeItem = document.createElement("p");
     nomeItem.innerText = inputItem.value;
 
     containerItemDaLista.appendChild(inputCheckbox);
     containerItemDaLista.appendChild(nomeItem);
     itemDaLista.appendChild(containerItemDaLista);
+
+    const diaDaSemana = new Date().toLocaleDateString('pt-BR', {
+        weekday: "long"
+    });
+    const data = new Date().toLocaleDateString('pt-BR');
+    const hora = new Date().toLocaleTimeString('pt-BR', {
+        hour: "numeric",
+        minute: "numeric"
+    });
+
+    const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`
+    const itemData = document.createElement('p');
+    itemData.innerText = dataCompleta;
+    itemData.classList.add('texto-data');
+
+    itemDaLista.appendChild(itemData);
     listaDeCompras.appendChild(itemDaLista);
 })
